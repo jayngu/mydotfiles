fb72d6bd61eb8dd253fc9c7cb977e3f4bf8e0a58
jayngu <jason.ly.nguyen@gmail.com>
2019-05-23 15:26:44 -0400

    Updated install script

--- install.sh
+++ install.sh
@@ -1,26 +1,8 @@
 #!/bin/bash
 # https://gist.github.com/dabroder/813a941218bdb164fb4c178d464d5c23
 
-sudo apt-get install -y \
-    libxcb1-dev \
-    libxcb-keysyms1-dev \
-    libpango1.0-dev \
-    libxcb-util0-dev \
-    libxcb-icccm4-dev \
-    libyajl-dev \
-    libstartup-notification0-dev \
-    libxcb-randr0-dev \
-    libev-dev \
-    libxcb-cursor-dev \
-    libxcb-xinerama0-dev \
-    libxcb-xkb-dev \
-    libxkbcommon-dev \
-    libxkbcommon-x11-dev \
-    autoconf \
-    libxcb-xrm0 \
-    libxcb-xrm-dev \ 
-    automake \
-    libxcb-shape0-dev
+sudo apt update
+sudo apt-get install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev
 
 cd /tmp
 
@@ -38,3 +20,10 @@ mkdir -p build && cd build/
 ../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
 make
 sudo make install
+
+
+# Install programs
+sudo apt-get install -y i3status i3lock feh compton ranger caca-utils
+
+# restart gdm or reboot;  doing this
+# systemctl restart gdm
