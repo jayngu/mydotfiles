9733006569f74c8f5cdfed29e116844d7777ae7a
jayngu <jason.ly.nguyen@gmail.com>
2019-05-24 06:16:57 -0400

    updated scripts

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # https://gist.github.com/dabroder/813a941218bdb164fb4c178d464d5c23
 
 sudo apt update
-sudo apt-get install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev
+sudo apt-get install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev i3status i3lock
 
 cd /tmp
 
@@ -21,9 +21,22 @@ mkdir -p build && cd build/
 make
 sudo make install
 
+# restart gdm (systemctl restart) or reboot machine
 
 # Install programs
-sudo apt-get install -y i3status i3lock feh compton ranger caca-utils udiskie
+sudo apt-get install -y feh dmenu arandr compton ranger caca-utils udiskie
 
-# restart gdm or reboot;  doing this
-# systemctl restart gdm
+# Install dependencies for polybar
+sudo apt-get install -y mpd cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev libxcb-composite0-dev
+
+cd /tmp
+
+# clone the repository for polybar and build from source
+git clone https://github.com/polybar/polybar.git
+cd polybar
+# run ./build.sh and follow instructions
+
+# After configuring polybar, run
+# $polybar example
+# Make sure it works and edit the config file for errors: ~/.config/polybar/config 
+
